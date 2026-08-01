bc009946480edede6c6aca4f191966bcd3f5545d
gabrielboudagh-boop <gabrielboudagh@gmail.com>
2026-08-01 09:02:16 -0400

    update

--- src/supabase.js
+++ src/supabase.js
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
   'https://vsmfnemgzyghremuenuh.supabase.co',
-  import.meta.env.VITE_SUPABASE_ANON_KEY
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZzbWZuZW1nenlnaHJlbXVlbnVoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk5NzgwMTIsImV4cCI6MjA5NTU1NDAxMn0.45T0X8OeZjgcu4HDdSI5LX-myNyE7QNBzTKmtLpRbf0'
 )
 
 
